e862b9f4c5da4a9d0ba30d7bafe8e7a8db92fecd
LiXiuFeng <1435808118@qq.com>
2018-11-24 23:13:54 +0800

    first commit

---
+++ blog/views.py
@@ -0,0 +1,116 @@
+from django.shortcuts import render, HttpResponse, redirect
+
+# Create your views here.
+
+from django.contrib import auth
+
+# from functools import wraps
+# from django.views import View
+
+from django.http import JsonResponse
+from geetest import GeetestLib  # 极验 geetest 模块
+
+# from django.contrib.auth.decorators import login_required
+#
+# # 注册相关
+# from django.contrib.auth.models import User
+#
+# # 用户注册相关
+from blog import forms, models
+
+# **************************** 使用极验滑动验证码的登录-开始 *************************
+
+# 请在官网申请ID使用，示例ID不可使用
+pc_geetest_id = "b46d1900d0a894591916ea94ea91bd2c"
+pc_geetest_key = "36fc3fe98530eea08dfc6ce76e3d24c4"
+
+
+def login(request):
+    # if request.is_ajax():  # 如果是AJAX请求
+    if request.method == "POST":
+        # 初始化一个给AJAX返回的数据
+        ret = {"status": 0, "msg": ""}
+        # 从提交过来的数据中 取到用户名和密码
+        username = request.POST.get("username")
+        pwd = request.POST.get("password")
+        print(username, pwd)
+        # 获取极验 滑动验证码相关的参数
+        gt = GeetestLib(pc_geetest_id, pc_geetest_key)
+        challenge = request.POST.get(gt.FN_CHALLENGE, '')
+        validate = request.POST.get(gt.FN_VALIDATE, '')
+        seccode = request.POST.get(gt.FN_SECCODE, '')
+        status = request.session[gt.GT_STATUS_SESSION_KEY]
+        print(status)
+        user_id = request.session["user_id"]
+
+        if status:
+            result = gt.success_validate(challenge, validate, seccode, user_id)
+        else:
+            result = gt.failback_validate(challenge, validate, seccode)
+        if result:
+            # 验证码正确
+            # 利用auth模块做用户名和密码的校验
+            user = auth.authenticate(username=username, password=pwd)
+            if user:
+                # 用户名密码正确
+                # 给用户做登录
+                auth.login(request, user)
+                ret["msg"] = "/index/"
+            else:
+                # 用户名密码错误
+                ret["status"] = 1
+                ret["msg"] = "用户名或密码错误！"
+        else:
+            ret["status"] = 1
+            ret["msg"] = "验证码错误"
+
+        return JsonResponse(ret)
+    return render(request, "login.html")
+
+
+# 处理极验 获取验证码的视图
+def get_geetest(request):
+    print("123")
+    user_id = 'test'
+    gt = GeetestLib(pc_geetest_id, pc_geetest_key)
+    status = gt.pre_process(user_id)
+    request.session[gt.GT_STATUS_SESSION_KEY] = status
+    request.session["user_id"] = user_id
+    response_str = gt.get_response_str()
+    return HttpResponse(response_str)
+
+
+# **************************** 使用极验滑动验证码的登录-结束 *************************
+
+
+def index(request):
+    return render(request, "index.html")
+
+
+# 用户注册的视图函数
+def register(request):
+    if request.method == "POST":
+        ret = {"status": 0, "msg": ""}
+        form_obj = forms.RegForm(request.POST)
+        print(request.POST)
+        # 帮我做校验
+        if form_obj.is_valid():
+            # 校验通过，去数据库创建一个新的用户
+            form_obj.cleaned_data.pop("re_password")
+            avatar_img = request.FILES.get("avatar")
+            models.UserInfo.objects.create_user(**form_obj.cleaned_data, avatar=avatar_img)
+            ret["msg"] = "/index/"
+            return JsonResponse(ret)
+        else:
+            print(form_obj.errors)
+            ret["status"] = 1
+            ret["msg"] = form_obj.errors
+            print(ret)
+            print("=" * 120)
+            return JsonResponse(ret)
+    # 生成一个form对象
+    form_obj = forms.RegForm()
+    print(form_obj.fields)
+    return render(request, "register.html", {"form_obj": form_obj})
+
+
